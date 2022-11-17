44d37b3cb30343692012afae6de84fad72004a4f
unknown <pauletteuwamahoro3@gmail.com>
2022-11-17 17:53:25 +0200

    first commit

---
+++ pages/forms/weight.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_REQUEST['we'])) {
+  # code...
+include_once 'connect.php';
+
+$num=$_REQUEST['serial'];
+$kg=$_REQUEST['weight'];
+
+$insert=$con->query("INSERT into weight(`weight`,Serial_No_Dev) values('$kg','$num')");
+if (!$insert) {
+echo "<script>alert('failed to insert')</script>";
+}
+else {
+  echo"well";
+}
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'almasaeed2010/adminlte',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'cakephp/chronos' => array(
+            'pretty_version' => '2.3.2',
+            'version' => '2.3.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cakephp/chronos',
+            'aliases' => array(),
+            'reference' => 'a21b7b633f483c4cf525d200219d200f551ee38b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
